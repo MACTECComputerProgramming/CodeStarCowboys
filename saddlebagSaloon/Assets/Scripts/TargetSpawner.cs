743972a29d21d5cfde009d72de76546c83cd6db9
Andrew_P <20abpayne@macon.k12.mo.us>
2019-10-17 12:05:02 -0500

    Implemented Character controller, shooting bottle mechanic and made the target spawner.

---
+++ saddlebagSaloon/Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+
+    public GameObject targetPrefab;
+    public Transform[] spawnPoints;
+
+    public float minDelay = .1f;
+    public float maxDelay = 1f;
+    public float timeBDestroy;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnTargets());
+    }
+
+    
+    IEnumerator SpawnTargets()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(1f);
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            GameObject spawnedTarget = Instantiate(targetPrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedTarget, timeBDestroy);
+        }
+    }
+}
