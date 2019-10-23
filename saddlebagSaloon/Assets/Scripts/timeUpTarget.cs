c9061fefb5f5cfe6c341b1947a412570bc089845
20CDPenland <35702052+20Dcpenland@users.noreply.github.com>
2019-10-23 13:34:45 -0500

    copied andrews files

---
+++ saddlebagSaloon/Assets/Scripts/timeUpTarget.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeUpTarget : MonoBehaviour
+{
+
+    public float health = 1f;
+    Rigidbody rb;
+    public float startingForce = 15;
+    public float timeBDestroy = 2f;
+    public float speedR;
+    public float timeAdd;
+    
+
+    
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * startingForce, ForceMode.Impulse);
+        transform.Rotate(90, 180, 0);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * speedR * Time.deltaTime);
+        Destroy(gameObject, timeBDestroy);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    
+    void Die()
+    {
+        
+        Destroy(gameObject);
+
+        GameTimer.currentTime = GameTimer.currentTime + timeAdd;
+    }
+}
