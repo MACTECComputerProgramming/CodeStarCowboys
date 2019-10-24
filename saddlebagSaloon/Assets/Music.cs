16ea1df73bb44e5ff6d2e5d5e2e447d30454c550
20CDPenland <35702052+20Dcpenland@users.noreply.github.com>
2019-10-24 13:42:37 -0500

    Main Music

---
+++ saddlebagSaloon/Assets/Music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip MainMusic;
+    public AudioSource MusicSource;
+    void Start()
+    {
+        MusicSource.clip = MainMusic;
+
+
+    }
+
+    
+    void Update()
+    {
+        MusicSource.Play();
+        if (true)
+        {
+
+        }
+    }
+}
