5176ea4193a4ac203df3f7199b567550241b9c08
zambia001 <43862219+zambia001@users.noreply.github.com>
2019-10-29 14:05:53 -0500

    merge?

---
+++ saddlebagSaloon/Assets/Scripts/scoreScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class scoreScript : MonoBehaviour
+{
+    public static float score = 16f;
+    
+    
+    [SerializeField]
+    public Text scoreText;
+
+    private void Start()
+    {
+        scoreText.text = score.ToString();
+
+      
+        
+    }
+
+    public static void addUp(float additive)
+    {
+        score = score + additive;
+    }
+
+}
---
+++ saddlebagSaloon/Assets/highScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highScore : MonoBehaviour
+{
+    [SerializeField]
+    public Text highScoreText;
+    private void Start()
+    {
+        highScoreText.text = "16";
+    }
+}
