0c1765bcbbe88049b12bf35a862336084fb83c5f
20CDPenland <35702052+20Dcpenland@users.noreply.github.com>
2019-10-30 14:11:32 -0500

    a

--- saddlebagSaloon/Assets/Scripts/Music.cs
+++ saddlebagSaloon/Assets/Scripts/Music.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioClip MainMusic;
+    public AudioSource MusicSource;
+
     void Start()
     {
-        
+        MusicSource.clip = MainMusic;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M)) ;
+        {
+            //MusicSource = !MusicSource;
+            MusicSource.Play();
+        }
     }
 }
