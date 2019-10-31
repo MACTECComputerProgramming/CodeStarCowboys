25d385af8b8ceef93117b5bc787104716fcf7e22
20CDPenland <35702052+20Dcpenland@users.noreply.github.com>
2019-10-31 14:15:28 -0500

    trying to fix scene manager

---
+++ saddlebagSaloon/Assets/Scripts/AudioBetweenScenes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBetweenScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private static AudioBetweenScenes instance = null;
+    public static AudioBetweenScenes Instance
+    {
+        get { return instance; }
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
